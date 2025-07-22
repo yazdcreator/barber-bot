ba8f8b7fe9791ac8385bab12d0d44bd83408e526
yazdcreator <yazedsattouf@gmail.com>
2025-07-22 16:11:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def time_chosen(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # تشغيل البوت
 if _name_ == '_main_':
     TOKEN = os.environ.get("BOT_TOKEN")
-    if not TOKEN:
+if not TOKEN:
     raise ValueError(" لم يتم العثور على BOT_TOKEN...")
     app = ApplicationBuilder().token(TOKEN).build()
 
